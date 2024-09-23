e7e1a4e7f660ac9de57168577d55c446643946da
Franco Cabral <francomaricabral@gmail.com>
2024-09-23 18:46:31 -0400

    Finished countrycodeconverter and inlabbyhandtranslator

--- src/main/java/org/translation/CountryCodeConverter.java
+++ src/main/java/org/translation/CountryCodeConverter.java
@@ -4,14 +4,19 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class provides the service of converting country codes to their names.
  */
 public class CountryCodeConverter {
 
-    // TODO Task: pick appropriate instance variable(s) to store the data necessary for this class
+    // Map to store Alpha-3 code as the key and country name as the value
+    private Map<String, String> alpha3ToCountryMap;
+    // Map to store country name as the key and Alpha-3 code as the value
+    private Map<String, String> countryToAlpha3Map;
 
     /**
      * Default constructor which will load the country codes from "country-codes.txt"
@@ -27,38 +32,46 @@ public class CountryCodeConverter {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public CountryCodeConverter(String filename) {
+        alpha3ToCountryMap = new HashMap<>();
+        countryToAlpha3Map = new HashMap<>();
 
         try {
+            // Load lines from the file
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
 
-            // TODO Task: use lines to populate the instance variable(s)
+            // Skip the header line and process the rest
+            for (int i = 1; i < lines.size(); i++) {
+                String[] parts = lines.get(i).split("\t");
+                String country = parts[0].trim();
+                String alpha3Code = parts[2].trim();
+                // Populate both maps
+                alpha3ToCountryMap.put(alpha3Code, country);
+                countryToAlpha3Map.put(country.toLowerCase(), alpha3Code);
+            }
 
         }
         catch (IOException | URISyntaxException ex) {
-            throw new RuntimeException(ex);
+            throw new RuntimeException("Failed to load country codes", ex);
         }
-
     }
 
     /**
-     * Returns the name of the country for the given country code.
-     * @param code the 3-letter code of the country
-     * @return the name of the country corresponding to the code
+     * Returns the name of the country for the given Alpha-3 country code.
+     * @param code the 3-letter Alpha-3 code of the country
+     * @return the name of the country corresponding to the code, or null if not found
      */
     public String fromCountryCode(String code) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return code;
+        return alpha3ToCountryMap.get(code);
     }
 
     /**
-     * Returns the code of the country for the given country name.
+     * Returns the Alpha-3 code of the country for the given country name.
      * @param country the name of the country
-     * @return the 3-letter code of the country
+     * @return the 3-letter Alpha-3 code of the country, or null if not found
      */
     public String fromCountry(String country) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return country;
+        return countryToAlpha3Map.get(country.toLowerCase());
     }
 
     /**
@@ -66,7 +79,6 @@ public class CountryCodeConverter {
      * @return how many countries are included in this code converter.
      */
     public int getNumCountries() {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return 0;
+        return alpha3ToCountryMap.size();
     }
 }
--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -3,12 +3,6 @@ package org.translation;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task: modify this class so that it also supports the Spanish language code "es" and
-//            one more language code of your choice. Each member of your group should add
-//            support for one additional langauge code on a branch; then push and create a pull request on GitHub.
-
-// Extra Task: if your group has extra time, you can add support for another country code in this class.
-
 /**
  * An implementation of the Translator interface which translates
  * the country code CANADA to several languages.
@@ -27,7 +21,8 @@ public class InLabByHandTranslator implements Translator {
     @Override
     public List<String> getCountryLanguages(String country) {
         if (CANADA.equals(country)) {
-            return new ArrayList<>(List.of("de", "en", "zh"));
+            // Adding support for Spanish ("es") and French ("fr")
+            return new ArrayList<>(List.of("de", "en", "zh", "es", "fr"));
         }
         return new ArrayList<>();
     }
@@ -57,14 +52,24 @@ public class InLabByHandTranslator implements Translator {
         }
 
         String translation = null;
-        if ("de".equals(language)) {
-            translation = "Kanada";
-        }
-        else if ("en".equals(language)) {
-            translation = "Canada";
-        }
-        else if ("zh".equals(language)) {
-            translation = "加拿大";
+        switch (language) {
+            case "de":
+                translation = "Kanada";
+                break;
+            case "en":
+                translation = "Canada";
+                break;
+            case "zh":
+                translation = "加拿大";
+                break;
+            case "es":
+                translation = "Canadá";
+                break;
+            case "fr":
+                translation = "Canada";
+                break;
+            default:
+                translation = null;
         }
 
         return translation;
